509ca0a7525930c3b8053a61e29b568b04712397
efraimgentil <efraim.gentil@gmail.com>
2017-02-20 19:52:56 -0300

    Initial

---
+++ src/main/java/me/efraimgentil/mymusic/MyMusicCatalogApplication.java
@@ -0,0 +1,12 @@
+package me.efraimgentil.mymusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMusicCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMusicCatalogApplication.class, args);
+	}
+}
